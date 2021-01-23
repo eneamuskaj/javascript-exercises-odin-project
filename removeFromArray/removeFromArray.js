9b1d31a21b060f6799bf874f415535d6f80a974b
Enea Muskaj <eneamuskaj@gmail.com>
2021-01-23 09:42:46 -0600

    first try to solve

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...argument) {
+    let newArray = Array.from(argument)
+    for (let i=0; i< array.length; i++) {
+        for ( let j=0; j<newArray.length; j++) {
+            if (array[i] === newArray[j]) {
+                array.splice(i, 1)
+        }
+        }
+    }
+    return array;
 }
-
 module.exports = removeFromArray
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = '';
+    for (let i=string.length-1; i>-1; i--) {
+        reversed += string[i];
+    }
+    return reversed;
 }
+
 
 module.exports = reverseString
