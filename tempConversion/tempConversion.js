da9ac6245ccc07a164ca9ce7e293535bb0c1f8b3
Enea Muskaj <eneamuskaj@gmail.com>
2021-01-23 10:43:49 -0600

    solved fa to celcius reverse challenge

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  result = (num -32) * 5/9
+  return Math.round(result*10)/10;
 }
 
-const ctof = function() {
-
+const ctof = function(num) {
+result2 = (num * 9/5) + 32;
+return Math.round(result2*10)/10;
 }
 
 module.exports = {
