519575f04607c8a91e94394059f9bef07292b85b
Enea Muskaj <eneamuskaj@gmail.com>
2021-01-28 20:19:30 -0600

    solved palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+    var clean = str.replace(/[^A-Z0-9]/ig, "");
+    cleanLower = clean.toLowerCase();
+    var cleanReverserd = cleanLower.split("").reverse().join("");
+    if (cleanReverserd === cleanLower) {
+        return true
+    } else {
+        return false
+    }
 }
 
 module.exports = palindromes
