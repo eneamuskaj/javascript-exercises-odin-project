97050cc4a4ac03919822f72c5eb6116c7cc67798
Enea Muskaj <eneamuskaj@gmail.com>
2021-01-29 22:22:46 -0600

    final solve caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -2,17 +2,27 @@ const caesar = function(str, num) {
     let arrayChar = [];
     let stringChar = [];
     splitted = str.split("");
+    if (num > 26) {
+        num = num % 26
+        console.log(num)
+    } else if (num < -26) {
+        num = num % 26
+    }
     for (let i=0; i<splitted.length; i++) {
         if ( (/[a-zA-Z]/).test(splitted[i])) {
-            if (splitted[i] === splitted[i].toUpperCase() && (splitted[i].charCodeAt(0) + num >90 )) {
+
+             if (splitted[i] === splitted[i].toLowerCase() && (splitted[i].charCodeAt(0) + num) > 122 ) {
+                arrayChar.push( (splitted[i].charCodeAt(0) + num) -26 )
+            }
+
+
+            else if (splitted[i] === splitted[i].toUpperCase() && (splitted[i].charCodeAt(0) + num > 90 )) {
                 arrayChar.push( (splitted[i].charCodeAt(0) + num) -26 )
             } else if
                  (splitted[i] === splitted[i].toUpperCase() && (splitted[i].charCodeAt(0) + num < 65 )) {
                  arrayChar.push( (splitted[i].charCodeAt(0) + num) +26 )
             }
-            else if (splitted[i] === splitted[i].toUpperCase() && (splitted[i].charCodeAt(0) + num) > 122 ) {
-                arrayChar.push( (splitted[i].charCodeAt(0) + num) -26 )
-            } else {
+             else {
             arrayChar.push(splitted[i].charCodeAt(0) + num);
             }
         } else {
