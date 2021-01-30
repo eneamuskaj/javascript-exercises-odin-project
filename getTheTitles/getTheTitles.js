711864ccc162aa061c6602cbeb0724e5ae21d466
Enea Muskaj <eneamuskaj@gmail.com>
2021-01-30 10:11:14 -0600

    soloved getTheTitle

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let array2 = array.map(function(element) {
+        return element.title
+    })
+    return array2
 }
 
 module.exports = getTheTitles;
