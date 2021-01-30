2423c1f70d2f3ab2b9fb2cd60a11a06bfacca3d3
Enea Muskaj <eneamuskaj@gmail.com>
2021-01-30 09:46:07 -0600

    solved fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    }
+    array=[1,1]
+    for (let i=1; i < num; i++) {
+        array.push(array[i] + array[i-1])
+        }
+  return array[num-1]
 }
 
 module.exports = fibonacci
