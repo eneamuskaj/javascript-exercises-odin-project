dae0bd7beb77d140dc364fc3d2dc7ec7ee4494f1
Enea Muskaj <eneamuskaj@gmail.com>
2021-01-29 21:05:22 -0600

    solved caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,26 @@
-const caesar = function() {
+const caesar = function(str, num) {
+    let arrayChar = [];
+    let stringChar = [];
+    splitted = str.split("");
+    for (let i=0; i<splitted.length; i++) {
+        if ( (/[a-zA-Z]/).test(splitted[i])) {
+            if ( (splitted[i].charCodeAt(0) + num) > 122 ) {
+                arrayChar.push( (splitted[i].charCodeAt(0) + num) -26 )
+            } else {
+            arrayChar.push(splitted[i].charCodeAt(0) + num);
+            }
+        } else {
+            arrayChar.push(splitted[i]);
+        }
+    }
+    for ( let j=0; j<arrayChar.length; j++) {
+        if (typeof arrayChar[j] === "number") {
+            stringChar.push(String.fromCharCode(arrayChar[j]))
+        } else {
+            stringChar.push(arrayChar[j])
+        }
+    }
+    return stringChar.join('')
 
 }
-
 module.exports = caesar
--- caesar/caesar.spec.js
+++ caesar/caesar.spec.js
@@ -4,7 +4,7 @@ describe('caesar', function() {
   it('works with single letters', function() {
     expect(caesar('A', 1)).toEqual('B');
   });
-  xit('works with words', function() {
+  it('works with words', function() {
     expect(caesar('Aaa', 1)).toEqual('Bbb');
   });
   xit('works with phrases', function() {
